5ad44d2d1ebcb39f17dde8ce29f30b1748455ca9
Булат Каримов <bulatkarimov96@gmail.com>
2020-05-04 17:41:31 +0300

    [add] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :public do
+      namespace :v1 do
+        resource :addresses, param: :address_uuid, only: :index
+
+        resource :polls, param: :poll_uuid, only: %i[index show create]
+      end
+    end
+  end
+
 end
