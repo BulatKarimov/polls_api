b280cbc15cd0ddb2becb98d9de2941d2c78d528e
Булат Каримов <bulatkarimov96@gmail.com>
2020-05-04 17:51:37 +0300

    [rm] set tokens

--- app/controllers/public/api/v1/application_controller.rb
+++ app/controllers/public/api/v1/application_controller.rb
@@ -1,22 +1,9 @@
 class Public::Api::V1::ApplicationController < Public::Api::ApplicationController
   include ActionController::Helpers
 
-  before_action :set_gis_token
-  before_action :set_ss_api_token
-
   def render_error(error: 'error.default', status: :unprocessable_entity)
     return render json: { error: error },
                   serializer: ::External::ErrorSerializer,
                   status: status
-  end
-
-  private
-
-  def set_ss_api_token
-    RequestStore.store[:ss_api_token] = "Bearer #{ENV['SS_API_TOKEN']}"
-  end
-
-  def set_gis_token
-    RequestStore.store[:gis_token] = "Bearer #{ENV['GIS_TOKEN']}"
   end
 end
