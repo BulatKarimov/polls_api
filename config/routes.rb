34b4789e4774661eb524496aca09d4d8709250b5
Булат Каримов <bulatkarimov96@gmail.com>
2020-05-04 17:48:45 +0300

    asdasdas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :api do
-    namespace :public do
+  namespace :public do
+    namespace :api do
       namespace :v1 do
         get 'addresses', to: 'addresses#index'
         resource :polls, param: :poll_uuid, only: %i[index show create]
