b4e59b6475f1594c9239529ee2865dfd76c7f8ed
Булат Каримов <bulatkarimov96@gmail.com>
2020-05-04 17:14:01 +0300

    [fix] 502

--- app/controllers/public/api/v1/addresses_controller.rb
+++ app/controllers/public/api/v1/addresses_controller.rb
@@ -1,8 +1,8 @@
 class Public::Api::V1::AddressesController < Public::Api::ApplicationController
 
-  expose :addresses, -> { Address.fetch_all }
-
   def index
+    addresses = Address.where(company_uuid: ENV['COMPANY_UUID'])
+
     render json: addresses, each_serializer: AddressSerializer, status: :ok
   end
 end
